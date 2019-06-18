f85ca4c5b063d95ab97c50fa7a4d177253e3f7b9
egin10 <egin066@gmail.com>
2019-06-18 18:06:50 +0700

    Updates

---
+++ precache-manifest.9c3ddd95e7cba4e3e70ca464526101dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b594852962d4ad2f8bf82a2b584b17f",
+    "url": "/simple-blog/index.html"
+  },
+  {
+    "revision": "69d61625d4a4dfff54a5",
+    "url": "/simple-blog/static/css/main.b5e9772a.chunk.css"
+  },
+  {
+    "revision": "856bac31f46c8ede3e81",
+    "url": "/simple-blog/static/js/2.a54cdf1a.chunk.js"
+  },
+  {
+    "revision": "69d61625d4a4dfff54a5",
+    "url": "/simple-blog/static/js/main.30f0c6b0.chunk.js"
+  },
+  {
+    "revision": "d1f0958660b969eb0fd4",
+    "url": "/simple-blog/static/js/runtime~main.5e2413b1.js"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/simple-blog/static/media/reactjs.47ce6e77.png"
+  }
+]);
