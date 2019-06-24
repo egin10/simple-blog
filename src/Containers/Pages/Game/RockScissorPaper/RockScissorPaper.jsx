a4d9fbb1aa2624944608f20567cc43d491d89996
egin10 <egin066@gmail.com>
2019-06-24 22:32:03 +0700

    add status win,lose&draw

--- src/Containers/Pages/Game/RockScissorPaper/RockScissorPaper.jsx
+++ src/Containers/Pages/Game/RockScissorPaper/RockScissorPaper.jsx
@@ -17,32 +17,49 @@ export default class RockScissorPaper extends Component {
         }
     }
 
-    handleRock = () => {
+    //HANDLER
+    handleChoice = (choice) => {
+        return this.checkChoice(choice)
+    }
+
+    //FUNCTION
+    checkChoice = (choice) => {
         let { arr } = this.state
+        let player = choice === 'ROCK' ? arr[0] : choice === 'SCISSOR' ? arr[1] : arr[2]
+        let com = arr[Math.floor(Math.random() * arr.length)]
+        // arr[0] = ROCK , arr[1] = SCISSOR , arr[2] = PAPER
+        let stats = (com === arr[2] && player === arr[0])
+            || (com === arr[0] && player === arr[1])
+            || (com === arr[1] && player === arr[2]) ? 'LOSE' :
+            (com === arr[1] && player === arr[0])
+                || (com === arr[2] && player === arr[1])
+                || (com === arr[0] && player === arr[2]) ? 'WIN' : 'DRAW'
         let result = {
-            show: arr[Math.floor(Math.random * arr.length)],
-            stats: "LOSE"
+            show: `(COM) ${com} <=== VS ===> ${player} (YOU)`,
+            stats: stats
         }
-        this.setState({ result: result }, () => this.state.result)
+        this.setState({ result: result })
     }
 
     render() {
         let { result } = this.state
         return (
             <div className="game-rsp">
-                <h2><span role="img" aria-label="Panda">ROCK✊ SCISSOR✌️ PAPER✋</span></h2>
+                <h2><span role="img" aria-label="rock-scissor-paper">ROCK✊ SCISSOR✌️ PAPER✋</span></h2>
                 <hr />
-                <p></p>
-                <button className="btn-choice" onClick={this.handleRock}>
+                <p>YOUR SCORE <span role="img" aria-label="rocket">🚀</span></p>
+                <button className="btn-choice" onClick={() => this.handleChoice('ROCK')}>
                     <span role="img" aria-label="Rock">ROCK✊</span>
                 </button>
-                <button className="btn-choice">
-                    <span role="img" aria-label="Panda">SCISSOR✌️</span>
+                <button className="btn-choice" onClick={() => this.handleChoice('SCISSOR')}>
+                    <span role="img" aria-label="Scissor">SCISSOR✌️</span>
                 </button>
-                <button className="btn-choice">
-                    <span role="img" aria-label="Panda">PAPER✋</span>
+                <button className="btn-choice" onClick={() => this.handleChoice('PAPER')}>
+                    <span role="img" aria-label="Paper">PAPER✋</span>
                 </button>
-                <p>{result.show}</p>
+                <hr />
+                <p className="result"><span role="img" aria-label="rock-scissor-paper">{result.show}</span></p>
+                <p className={result.stats}>{result.stats}</p>
             </div>
         )
     }
